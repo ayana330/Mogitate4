14597d4458ba4bcee8afbee9030951f16f3bde01
ayana <wencaishanben@gmail.com>
2025-06-13 14:47:59 +0900

    一覧

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,14 +1,57 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>mogitate</title>
 </head>
 <body>
+    <h1>商品一覧</h1>
+    <div class="flex"></div>
+    <a class="practice__btn"></a>
     <div class="fruit">
       <div class="fruit__img">
-        <img src="img/fruit.jpg" alt="">
+        <img src="/storage/img/kiwi.png" alt="kiwi">
+      </div>
+      <div class="card__ttl">
+        キウイ ￥800
+      </div>
+        <img src="/storage/img/strawberry.png" alt="strawberry">
+      <div class="card__ttl">
+        ストロベリー  ￥1200
+      </div>
+        <img src="/storage/img/orange.png" alt="orange">
+      <div class="card__ttl">
+        オレンジ  ￥850
+      </div>
+        <img src="/storage/img/watermelon.png" alt="watermelon">
+      <div class="card__ttl">
+        スイカ  ￥700
+      </div>
+        <img src="/storage/img/peach.png" alt="peach">
+      <div class="card__ttl">
+        ピーチ  ￥1000
+      </div>
+        <img src="/storage/img/muscat.png" alt="muscat">
+      <div class="card__ttl">
+        シャインマスカット  ￥1400
+      </div>
+        <img src="/storage/img/banana.png" alt="banana">
+        バナナ  ￥600
+      </div>
+        <img src="/storage/img/grapes.png" alt="grapes">
+        ぶどう  ￥1100
+      </div>
+        <img src="/storage/img/melon.png" alt="melon">
+        メロン  ￥900
+      </div>
+        <img src="/storage/img/pineapple.png" alt="pineapple">
+        パイナップル  ￥800
+      </div>
+
+
+
+
     </div>
 </body>
 </html>

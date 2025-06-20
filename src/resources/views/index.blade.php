81f24475ea9cca4df8c00950b5f94ccb36e493d4
ayana <wencaishanben@gmail.com>
2025-06-20 14:19:38 +0900

    0620

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,90 +1,116 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>mogitate</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/css/sanitize.css">
+  <link rel="stylesheet" href="/css/common.css">
+  <link rel="stylesheet" href="/css/index.css">
+  <title>mogitate</title>
+
 </head>
 
 <body>
+<h1 class="header__logo">
+        mogitate
+      </h1>
+
   <header class="header">
     <div class="header__inner">
-      <a class="header__logo">
-        mogitate
-      </a>
+      <h2>商品一覧</h2>
+      </div>  
+    <div>
+      <a href="/products/register">＋商品を追加</a>
     </div>
   </header>
-  <main>
-  <div class="flex">
-   <div class="search-form__content">
-    <h2 class="search-form__content-title">商品一覧</h2>
-   </div>
-   <form class="search-form" action="public/img" method="get"></form>
-    <div class="product__group-content">
-      <div class="product__input--text">
-        <input type="text" name="name" placeholder="商品名で検索" />
-      </div>
-    </div>
-    <div class="product__button">
-      <button class="product__button-submit" type="submit">検索</button>
-    </div>
-    <h4 class="search-form__content-tittle">価格順で表示</h4>
-    <form action="" method=""></form>
-      <select name="select">
-        <option value="高い順に表示">高い順に表示</option>
-        <option value="低い順に表示">低い順に表示</option>
-      </select>
-      
-   <div class="flex__item">
-    <div class="practice__card">
-      <div class="fruit__img">
-        <img src="/storage/img/kiwi.png" alt="kiwi">
-      </div>
-      <div class="card__ttl">
-        キウイ ￥800
-      </div>
-      <div class="fruit__img">
-        <img src="/storage/img/strawberry.png" alt="strawberry">
-      <div class="card__ttl">
-        ストロベリー  ￥1200
-      </div>
-      <div class="fruit__img">
-        <img src="/storage/img/orange.png" alt="orange">
-      <div class="card__ttl">
-        オレンジ  ￥850
-      </div>
-      <div class="fruit__img">
-        <img src="/storage/img/watermelon.png" alt="watermelon">
-      <div class="card__ttl">
-        スイカ  ￥700
-      </div>
-      <div class="fruit__img">
-        <img src="/storage/img/peach.png" alt="peach">
-      <div class="card__ttl">
-        ピーチ  ￥1000
-      </div>
-      <div class="fruit__img">
-        <img src="/storage/img/muscat.png" alt="muscat">
-      <div class="card__ttl">
-        シャインマスカット  ￥1400
-      </div>
-      <div class="fruit__img">
-        <img src="/storage/img/banana.png" alt="banana">
-        バナナ  ￥600
-      </div>
-      <div class="fruit__img">
-        <img src="/storage/img/grapes.png" alt="grapes">
-        ぶどう  ￥1100
-      </div>
-      <div class="fruit__img">
-        <img src="/storage/img/melon.png" alt="melon">
-        メロン  ￥900
-      </div>
-      <div class="fruit__img">
-        <img src="/storage/img/pineapple.png" alt="pineapple">
-        パイナップル  ￥800
-      </div>
-    </div>
-</div>
+  <main class="container">
+    
+    <div class="flex">
+      <aside>
+        <form class="search-form" action="public/img" method="get">
+          <div class="product__group-content">
+            <div class="product__input--text">
+              <input type="text" name="name" placeholder="商品名で検索" />
+            </div>
+          </div>
+          <div class="product__button">
+            <button class="product__button-submit" type="submit">検索</button>
+          </div>
+        </form>
+
+        <h4 class="search-form__content-tittle">価格順で表示</h4>
+        <form action="" method="">
+        <select name="select" >
+          <option value="">価格で並べ替え</option>
+          <option value="高い順に表示">高い順に表示</option>
+          <option value="低い順に表示">低い順に表示</option>
+        </select>
+        </form>
+      </aside>
+      <div class="list">
+        <div class="flex__item">
+          <div class="practice__card">
+            <div class="fruit__img">
+              <img src="/storage/img/kiwi.png" alt="kiwi">
+            </div>
+            <div class="card__ttl">
+              キウイ ￥800
+            </div>
+          </div>
+          <div class="practice__card">
+            <div class="fruit__img">
+              <img src="/storage/img/strawberry.png" alt="strawberry">
+            </div>
+            <div class="card__ttl">
+                ストロベリー ￥1200
+              </div>
+          </div>
+          <div class="practice__card">
+            <div class="fruit__img">
+              <img src="/storage/img/strawberry.png" alt="strawberry">
+            </div>
+            <div class="card__ttl">
+                ストロベリー ￥1200
+              </div>
+          </div>
+          <div class="practice__card">
+            <div class="fruit__img">
+              <img src="/storage/img/strawberry.png" alt="strawberry">
+            </div>
+            <div class="card__ttl">
+                ストロベリー ￥1200
+              </div>
+          </div>
+          <div class="practice__card">
+            <div class="fruit__img">
+              <img src="/storage/img/strawberry.png" alt="strawberry">
+            </div>
+            <div class="card__ttl">
+                ストロベリー ￥1200
+              </div>
+          </div>
+          <div class="practice__card">
+            <div class="fruit__img">
+              <img src="/storage/img/strawberry.png" alt="strawberry">
+            </div>
+            <div class="card__ttl">
+                ストロベリー ￥1200
+              </div>
+          </div>
+          <div class="practice__card">
+            <div class="fruit__img">
+              <img src="/storage/img/strawberry.png" alt="strawberry">
+            </div>
+            <div class="card__ttl">
+                ストロベリー ￥1200
+              </div>
+          </div>
+
+                    </div><!-- end flex__item -->
+          </div><!-- end list -->
+      </div><!-- end flex -->
+  </main>
 </body>
+
 </html>

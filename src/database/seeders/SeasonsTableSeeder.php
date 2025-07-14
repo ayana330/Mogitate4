40ae35673600fad0f265e741f42c79a9cbd34ae4
ayana <wencaishanben@gmail.com>
2025-07-14 10:01:19 +0900

    0714

--- src/database/seeders/SeasonsTableSeeder.php
+++ src/database/seeders/SeasonsTableSeeder.php
@@ -14,28 +14,13 @@ class SeasonsTableSeeder extends Seeder
      */
     public function run()
     {
-        $param = [
-            'id' => '1',
-            'name' => '春',
-        ];
-        DB::table('seasons')->insert($param);
+        DB::table('seasons')->truncate();
 
-        $param = [
-            'id' => '2',
-            'name' => '夏',
-        ];
-        DB::table('seasons')->insert($param);
-
-        $param = [
-            'id' => '3',
-            'name' => '秋',
-        ];
-        DB::table('seasons')->insert($param);
-        
-        $param = [
-            'id' => '4',
-            'name' => '冬',
-        ];
-        DB::table('seasons')->insert($param);
+        DB::table('seasons')->insert([
+            ['id' => 1, 'name' => '春'],
+            ['id' => 2, 'name' => '夏'],
+            ['id' => 3, 'name' => '秋'],
+            ['id' => 4, 'name' => '冬'],
+        ]);
     }
 }

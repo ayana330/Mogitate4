ad7d2a14d6518aa70add4d7f77b3d87df16e93a6
ayana <wencaishanben@gmail.com>
2025-07-22 10:32:28 +0900

    0722

--- src/app/Models/Product.php
+++ src/app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
@@ -11,4 +12,11 @@ class Product extends Model
 
     protected $fillable = ['name', 'price', 'image', 'season', 'description'
     ];
+}
+
+class Post extends Model
+{
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
 }
